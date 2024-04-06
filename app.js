f06d9f6835649c2e960f58ad65704979a2d904ea
Raghav <raghav.rathi6634@gmail.com>
2024-04-07 02:13:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+
+
+
+
+const port = 3000
+app.listen(port,()=>{
+    console.log(`App listinng on port ${port}`)
+})
