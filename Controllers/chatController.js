a1705992387cb79fb9a46d7079c57e26d87ef3f9
Utsav Tomar <95949906+zitsav@users.noreply.github.com>
2024-04-08 00:05:26 +0530

    chat

--- Controllers/chatController.js
+++ Controllers/chatController.js
@@ -41,10 +41,30 @@ async function sendMessage(req, res) {
 }
 
 async function getMessages(req, res) {
-    const { senderId, receiverId } = req.query;
+    const { senderId, receiverId } = req.body;
   
     try {
-      // Fetch messages based on senderId and receiverId
+      // Check if senderId and receiverId are provided in the query parameters
+
+      if (!senderId || !receiverId) {
+        return res.status(400).json({ error: 'Missing senderId or receiverId in request' });
+      }
+  
+      // Find sender and receiver by their IDs
+      const sender = await prisma.user.findUnique({
+        where: { id: senderId },
+      });
+  
+      const receiver = await prisma.user.findUnique({
+        where: { id: receiverId },
+      });
+  
+      // Check if sender or receiver is not found
+      if (!sender || !receiver) {
+        return res.status(404).json({ error: 'Sender or receiver not found' });
+      }
+  
+      // Find messages sent between senderId and receiverId
       const messages = await prisma.message.findMany({
         where: {
           OR: [
@@ -52,15 +72,18 @@ async function getMessages(req, res) {
             { senderId: receiverId, receiverId: senderId },
           ],
         },
-        orderBy: { createdAt: 'asc' },
+        orderBy: {
+          createdAt: 'asc', // Order messages by createdAt in ascending order
+        },
       });
   
+      // Respond with the fetched messages
       res.status(200).json({ messages });
     } catch (error) {
       console.error('Error fetching messages:', error);
       res.status(500).json({ error: 'Failed to fetch messages' });
     }
-}
+  }
     
 module.exports = {
   sendMessage,
--- Controllers/loggedInUserController.js
+++ Controllers/loggedInUserController.js
@@ -147,5 +147,6 @@ module.exports = {
   getUserProfile,
   getUserGuidedTours,
   getSingleTour,
-  paymentURLGeneration
+  paymentURLGeneration,
+  writeAReview
 }
